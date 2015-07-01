27f511f18c26eb5f0aa940098ef8f4dee5e98a86
Bin Chen <chenbin11200+git@gmail.com>
2015-07-01 14:34:42 +0200

    4. 定制长宽和位置
    customize width and height

--- js/window.js
+++ js/window.js
@@ -1,11 +1,14 @@
 define(['jquery'],function($){
 
     function Window(){
-
+        this.cfg={
+            width: 500,
+            height: 300
+        };
     }
 
     Window.prototype={
-        alert: function(content,handler){
+        alert: function(content,handler,cfg){
             var boundingBox = $('<div class="window_boundingBox"></div>');
             boundingBox.appendTo("body");
             boundingBox.html(content);
@@ -15,6 +18,13 @@ define(['jquery'],function($){
          		handler && handler();
         		boundingBox.remove();
     	    });
+            $.extend(this.cfg,cfg);
+            boundingBox.css({
+                width : this.cfg.width + "px",
+                height : this.cfg.height + "px",
+                left : (this.cfg.x || (window.innerWidth - this.cfg.width)/2) + "px",
+                top : (this.cfg.y || (window.innerHeight - this.cfg.height)/2) + "px"
+            });
         },
         confirm:function(){
 
